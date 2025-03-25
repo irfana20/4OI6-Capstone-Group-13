840846f7f6faa77d86bb306fccc56192e477e3d5
claraabadir <abadirc@mcmaster.ca>
2025-03-25 08:26:39 -0400

    InitApp

--- initialize_app.py
+++ initialize_app.py
@@ -5,7 +5,7 @@ class InitApp:
 
     def __init__(self):
         # Initialize Firebase Admin SDK
-        cred = credentials.Certificate("capstone-app-59cf7.json")
+        cred = credentials.Certificate("../capstone-app-59cf7.json")
         firebase_admin.initialize_app(cred, {
             'storageBucket': "capstone-app-59cf7.appspot.com",
             'databaseURL': 'https://capstone-app-59cf7.firebaseio.com/'
