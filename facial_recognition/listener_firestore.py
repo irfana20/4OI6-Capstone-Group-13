895bc915091a8feb191a1046ac12e209b8140cbe
irfana20 <89694766+irfana20@users.noreply.github.com>
2025-03-25 09:45:52 -0400

    Update listener_firestore.py

--- facial_recognition/listener_firestore.py
+++ facial_recognition/listener_firestore.py
@@ -1,18 +1,11 @@
-import firebase_admin
-from firebase_admin import credentials, firestore, storage
 import urllib.request
 import os
+import sys
 import time
 from train_faces import train_faces  # Your existing training function
 
-# Initialize Firebase Admin SDK
-cred = credentials.Certificate("capstone-app-59cf7.json")
-firebase_admin.initialize_app(cred, {
-    'storageBucket': "capstone-app-59cf7.appspot.com"
-})
-
-db = firestore.client()
-bucket = storage.bucket()
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from initialize_app import InitApp
 
 # Track which photos have been processed
 def load_processed_photos():
