8f7d445c8b269f928c2e238554b1bf3ec6d06f6e
irfana20 <89694766+irfana20@users.noreply.github.com>
2025-03-25 09:47:23 -0400

    Update initialize_app.py

--- initialize_app.py
+++ initialize_app.py
@@ -1,18 +1,25 @@
 import firebase_admin
+import os
 from firebase_admin import credentials, firestore, storage
 
 class InitApp:
 
     def __init__(self):
+        self.db = None
+        self.bucket = None
+        
+    
+    def main(self):
         # Initialize Firebase Admin SDK once
         if not firebase_admin._apps:
-            cred = credentials.Certificate("../capstone-app-59cf7.json")
+            cred_path = os.path.join(os.path.dirname(__file__),"capstone-app-59cf7-firebase-adminsdk-62bt7-539d8787a6.json")
+            cred = credentials.Certificate(cred_path)
             firebase_admin.initialize_app(cred, {
                 'storageBucket': "capstone-app-59cf7.appspot.com",
                 'databaseURL': 'https://capstone-app-59cf7.firebaseio.com/'
             })
 
-        db = firestore.client()
-        bucket = storage.bucket()
+        self.db = firestore.client()
+        self.bucket = storage.bucket()
 
-        return db, bucket
+        return self.db, self.bucket
